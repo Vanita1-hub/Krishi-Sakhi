04d3cf63f1e49d64d6b9d0b57864f3114de7e816
Vanita Ranjan <vanitaranjan2@gmail.com>
2026-07-21 16:36:28 +0530

    Update vite.config.js

--- public/vite.config.js
+++ public/vite.config.js
@@ -1,7 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' // Or your specific framework plugin
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/Krishi-Sakhi/', // ⚠️ Replace this with your actual GitHub repo name
-})
